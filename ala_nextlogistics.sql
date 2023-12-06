23897c3733df5df1949970679b1b0405f9d0e1bd
Diana <1005201@mborijnland.nl>
2023-12-06 10:00:00 +0100

    update database heb entrydt, workdt, startdt,enddt aangepast

--- ala_nextlogistics.sql
+++ ala_nextlogistics.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 27 nov 2023 om 09:47
+-- Gegenereerd op: 06 dec 2023 om 09:53
 -- Serverversie: 10.4.27-MariaDB
 -- PHP-versie: 8.2.0
 
@@ -33,10 +33,20 @@ CREATE TABLE `project` (
   `Code` int(11) NOT NULL,
   `Actual` tinyint(1) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `StartDT` datetime NOT NULL,
-  `EndDT` datetime NOT NULL,
+  `StartDT` time NOT NULL,
+  `EndDT` time NOT NULL,
   `MaxHours` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `project`
+--
+
+INSERT INTO `project` (`ID`, `Active`, `Code`, `Actual`, `Title`, `StartDT`, `EndDT`, `MaxHours`) VALUES
+(1, 1, 9909, 1, 'SensorData', '11:01:51', '11:01:51', '00:00:50'),
+(2, 1, 4564, 1, 'DataCake', '11:01:51', '11:01:51', '00:00:50'),
+(3, 1, 3456, 1, 'GroeneDaken', '11:07:02', '11:07:02', '00:00:50'),
+(4, 1, 1321, 1, 'Sopranos', '11:07:02', '11:07:02', '00:00:50');
 
 -- --------------------------------------------------------
 
@@ -48,10 +58,33 @@ CREATE TABLE `projectdata` (
   `ID` int(11) NOT NULL,
   `ProjectID` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
-  `EntryDT` datetime NOT NULL,
-  `WorkDT` datetime NOT NULL,
+  `EntryDT` date NOT NULL,
+  `WorkDT` date NOT NULL,
   `Description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `projectdata`
+--
+
+INSERT INTO `projectdata` (`ID`, `ProjectID`, `UserID`, `EntryDT`, `WorkDT`, `Description`) VALUES
+(1, 1, 1, '2023-11-27', '2023-11-30', 'Data for sensors'),
+(2, 2, 2, '2023-11-27', '2023-11-30', 'Bunch of data stuff'),
+(3, 3, 1, '2023-11-27', '2023-11-27', 'Website met sensoren die het weer meten'),
+(4, 4, 2, '2023-11-27', '2023-11-27', 'Pizza Website'),
+(5, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(6, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(7, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(8, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(9, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(10, 1, 1, '2023-12-04', '2023-12-04', 'Werkzaamheden voltooid.'),
+(11, 2, 1, '2023-12-04', '0000-00-00', 'help met form'),
+(12, 2, 1, '2023-12-04', '0000-00-00', 'help met form'),
+(13, 2, 1, '2023-12-04', '0000-00-00', 'help met form'),
+(14, 4, 1, '2023-12-04', '0000-00-00', 'form werkt nog niet'),
+(15, 4, 1, '2023-12-04', '0000-00-00', 'form werkt voor de helft'),
+(16, 4, 2, '2023-12-04', '0000-00-00', 'form werkt voor de helft'),
+(17, 4, 1, '2023-12-04', '0000-00-00', 'form werkt voor de helft');
 
 -- --------------------------------------------------------
 
@@ -66,6 +99,16 @@ CREATE TABLE `projectusers` (
   `MayManage` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `projectusers`
+--
+
+INSERT INTO `projectusers` (`ID`, `ProjectID`, `UserID`, `MayManage`) VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 0),
+(3, 3, 1, 1),
+(4, 4, 2, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +120,14 @@ CREATE TABLE `users` (
   `Active` tinyint(1) NOT NULL,
   `Name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`ID`, `Active`, `Name`) VALUES
+(1, 1, 'Quinten van Meijeren'),
+(2, 1, 'Diana van Kampen');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -100,6 +151,7 @@ ALTER TABLE `projectdata`
 -- Indexen voor tabel `projectusers`
 --
 ALTER TABLE `projectusers`
+  ADD PRIMARY KEY (`ID`),
   ADD KEY `UserID` (`UserID`),
   ADD KEY `ProjectID` (`ProjectID`);
 
@@ -117,19 +169,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `project`
 --
 ALTER TABLE `project`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT voor een tabel `projectdata`
 --
 ALTER TABLE `projectdata`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT voor een tabel `projectusers`
+--
+ALTER TABLE `projectusers`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
